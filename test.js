857a2df862ce2d7afab7c1b048a38f79170ac2a3
Mohib-Akram <muhammadmohibakram@gmail.com>
2026-08-10 17:20:29 +0500

    Remove secrets and node_modules

--- test.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Test server chal raha hai!'));
-app.listen(3000, () => console.log('Test server ready on port 3000'));
